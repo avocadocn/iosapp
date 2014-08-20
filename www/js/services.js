b7a3973b380ed7b8a8749c35e5c14c0b8ecddc8f
afei <afei@55yali.com>
2014-08-20 11:58:39 +0800

    修改baseurl

--- www/js/services.js
+++ www/js/services.js
@@ -6,8 +6,8 @@ angular.module('starter.services', [])
 
 
 .factory('Global', function() {
-  var base_url = window.location.origin;
-  //var base_url = "http://www.donler.com";
+  //var base_url = window.location.origin;
+  var base_url = "http://www.donler.com";
   var _user = {};
   var last_date;
 
