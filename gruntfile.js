25df2365abca7fb677eea0baf2b66803bec0833b
cahavar <cahavar@55yali.com>
2014-08-28 19:13:50 +0800

    添加stylus配置

---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function(grunt) {
+    // Project Configuration
+    grunt.initConfig({
+        stylus: {
+            compile: {
+                options: {
+                    urlfunc: 'embedurl',
+                    compress: false
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'www/css/',
+                    src: '*.styl',
+                    dest: 'www/css/',
+                    ext: '.css'
+                }]
+            }
+        }
+    });
+
+    require('load-grunt-tasks')(grunt);
+
+    //Making grunt default to force in order not to break the project.
+    grunt.option('force', true);
+
+    //Default task(s).
+
+    grunt.registerTask('default', ['stylus']);
+
+};
+
+
