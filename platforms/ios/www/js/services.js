7a85c4a239a5095513ad59d62f8017c9436a37a0
vitrum.zhu <vitrum.cn@gmail.com>
2014-08-29 17:57:07 +0800

    update icon

--- platforms/ios/www/js/services.js
+++ platforms/ios/www/js/services.js
@@ -9,14 +9,11 @@ angular.module('starter.services', [])
   //var base_url = window.location.origin;
   //var base_url = "http://www.donler.com";
   var base_url = "http://www.55yali.com";
-
-  //var base_url = "http://192.168.2.106:3000";
-  //var base_url = "http://192.168.2.101:3000";
-  ionic.Platform.ready(function(){
-    window.plugin.notification.local.onclick = function (id, state, json) {
-      $state.go('app.campaignDetail',{'id':JSON.parse(json).id});
-    };
-  });
+  // ionic.Platform.ready(function(){
+  //   window.plugin.notification.local.onclick = function (id, state, json) {
+  //     $state.go('app.campaignDetail',{'id':JSON.parse(json).id});
+  //   };
+  // });
   var _user = {};
   var last_date;
   return {
@@ -51,14 +48,75 @@ angular.module('starter.services', [])
 
   var login = function($scope) {
     return function(username, password) {
-      function loginPost(ids,token){
+
+      function initPushwoosh(callback) {
+        var pushNotification = window.plugins.pushNotification;
+        console.log('Received Event: ');
+        console.warn(pushNotification);
+        if(!window.plugins){
+          callback(null,null,'NO_WINDOW_PLUGINS');
+        }else{
+          if(!window.plugins.pushNotification){
+            callback(null,null,'NO_PUSHNOTIFICATION');
+          }else{
+            //set push notification callback before we initialize the plugin
+            document.addEventListener('push-notification', function(event) {
+                          //get the notification payload
+                          var notification = event.notification;
+
+                          //display alert to the user for example
+                          alert(notification.aps.alert);
+                          //clear the app badge
+                          pushNotification.setApplicationIconBadgeNumber(0);
+                        });
+
+              //initialize the plugin
+              pushNotification.onDeviceReady({pw_appid:"B13D4-3532F"});
+
+              //register for pushes
+              pushNotification.registerDevice(function(status) {
+                                                    var deviceToken = status['deviceToken'];
+                                                    console.warn('registerDevice: ' + deviceToken);
+                              },
+                              function(status) {
+                                                    console.warn('failed to register : ' + JSON.stringify(status));
+                                                    navigator.notification.alert(JSON.stringify(['failed to register ', status]));
+                                                    callback(null,null,status);
+                              });
+              pushNotification.setApplicationIconBadgeNumber(0);
+              pushNotification.getTags(function(tags) {
+                            console.warn('tags for the device: ' + JSON.stringify(tags));
+                           },
+                           function(error) {
+                            console.warn('get tags error: ' + JSON.stringify(error));
+                           });
+
+              pushNotification.getPushToken(function(token) {
+                              console.warn('push token device: ' + token);
+                              //执行loginPost,将token POST到后台
+                              callback(null,token);
+                           });
+
+              pushNotification.getPushwooshHWID(function(token) {
+                              console.warn('Pushwoosh HWID: ' + token);
+                            });
+
+              //start geo tracking.
+              pushNotification.startLocationTracking(function() {
+                                                     console.warn('Location Tracking Started');
+                                                     });
+          }
+        }
+      }
+      function loginPost(ids,token,_status){
         $http.post(Global.base_url + '/users/login', { 
           username: username,
           password: password,
           device: ionic.Platform.device(),
           userid: ids ? ids[0] : '',
           channelid: ids ? ids[1] : '',
-          token: token ? token : ''
+          token: token ? token : '',
+          status:_status
         })
         .success(function(data, status, headers, config) {
             if (data.result === 1) {
@@ -101,8 +159,8 @@ angular.module('starter.services', [])
       }
       else{
         //console.log(ionic.Platform.device().platform);
-        //initPushwoosh(loginPost);
-        loginPost(null,null,'NULL',null);
+        initPushwoosh(loginPost);
+        //loginPost(null,null,'NULL',null);
       }
     };
   };
@@ -146,58 +204,6 @@ angular.module('starter.services', [])
       }
     });
   };
-
-  function initPushwoosh(callback) {
-    var pushNotification = window.plugins.pushNotification;
-    console.log('Received Event: ');
-    console.warn(pushNotification);
-    //set push notification callback before we initialize the plugin
-    document.addEventListener('push-notification', function(event) {
-                  //get the notification payload
-                  var notification = event.notification;
-
-                  //display alert to the user for example
-                  alert(notification.aps.alert);
-                  //clear the app badge
-                  pushNotification.setApplicationIconBadgeNumber(0);
-                });
-
-      //initialize the plugin
-      pushNotification.onDeviceReady({pw_appid:"B13D4-3532F"});
-
-      //register for pushes
-      pushNotification.registerDevice(function(status) {
-                                            var deviceToken = status['deviceToken'];
-                                            console.warn('registerDevice: ' + deviceToken);
-                      },
-                      function(status) {
-                                            console.warn('failed to register : ' + JSON.stringify(status));
-                                            navigator.notification.alert(JSON.stringify(['failed to register ', status]));
-                                            callback(null,null,'ERROR',status);
-                      });
-      pushNotification.setApplicationIconBadgeNumber(0);
-      pushNotification.getTags(function(tags) {
-                    console.warn('tags for the device: ' + JSON.stringify(tags));
-                   },
-                   function(error) {
-                    console.warn('get tags error: ' + JSON.stringify(error));
-                   });
-
-      pushNotification.getPushToken(function(token) {
-                      console.warn('push token device: ' + token);
-                      //执行loginPost,将token POST到后台
-                      callback(null,token,'SUCCESS',null);
-                   });
-
-      pushNotification.getPushwooshHWID(function(token) {
-                      console.warn('Pushwoosh HWID: ' + token);
-                    });
-
-      //start geo tracking.
-      pushNotification.startLocationTracking(function() {
-                                             console.warn('Location Tracking Started');
-                                             });
-  }
   return {
     authorize: authorize,
     login: login,
@@ -338,7 +344,7 @@ angular.module('starter.services', [])
     return function(campaign,tid) {
       $http.post(Global.base_url + '/campaign/joinCampaign/'+campaign._id, { campaign_id: campaign._id, tid:tid})
       .success(function(data, status, headers, config) {
-        scheduleCampaign(campaign);
+        // scheduleCampaign(campaign);
         callback(campaign._id);
       });
     };
@@ -350,7 +356,7 @@ angular.module('starter.services', [])
       $http.post(Global.base_url + '/campaign/quitCampaign/'+id, { campaign_id: id})
       .success(function(data, status, headers, config) {
         callback(id);
-        cancelScheduleCampaign(id);
+        // cancelScheduleCampaign(id);
       });
     };
   };
