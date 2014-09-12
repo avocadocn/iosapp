0c12dad028df6596da0ca66546bb29825345ed04
afei <afei@55yali.com>
2014-09-12 14:08:46 +0800

    修改index.html,修改权限认证系统

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,47 @@
 angular.module('starter.controllers', ['ngTouch', 'ionic.contrib.ui.cards'])
 
 
-.controller('AppCtrl', function($state, $scope, $rootScope, Authorize, Global) {
+.controller('AppCtrl', function($state, $scope, $rootScope, $ionicPopup, Authorize, Global) {
   if (Authorize.authorize() === true) {
     $state.go('app.index');
   }
+  console.log(Global);
   $scope.logout = Authorize.logout;
   $scope.base_url = Global.base_url;
   $scope.img_url = Global.img_url;
   $scope.user = Global.user;
+  document.addEventListener("resume", function() {
+    window.plugins.pushNotification.setApplicationIconBadgeNumber(0);
+    Authorize.autologin(false,function(loginStatus,otherStatus){
+      if(!loginStatus){
+        if(otherStatus){
+          var myPopup = $ionicPopup.show({
+            template: '您的账号在其他设备上登录，请重新登录!',
+            title: '警告',
+            scope: $scope,
+            buttons: [
+              { text: '取消' },
+              {
+                text: '<b>确定</b>',
+                type: 'button-positive',
+                onTap: function(e) {
+                  return true;
+                }
+              },
+            ]
+          });
+          myPopup.then(function(res) {
+            if(res){
+              $state.go('login');
+            }
+          });
+        }
+        else{
+          $state.go('login');
+        }a
+      }
+    });
+  }, false);
   
 })
 
@@ -18,7 +51,14 @@ angular.module('starter.controllers', ['ngTouch', 'ionic.contrib.ui.cards'])
     $state.go('app.index');
   }
   else {
-    $scope.checkStatus = true;
+    Authorize.autologin(true,function(loginStatus){
+      if(loginStatus){
+        $state.go('app.index');
+      }
+      else{
+        $scope.checkStatus = true;
+      }
+    });
   }
   $scope.data = {
     username: '',
