f581aae5e34dd13498aa7526366a55667670908f
vitrum <vitrum.cn@gmail.com>
2014-12-03 15:06:47 +0800

    update

---
+++ www/js/services.js
@@ -0,0 +1,55 @@
+
+/**
+ * Created by Sandeep on 11/09/14.
+ */
+angular.module('todoApp.services',[]).factory('Todo',['$http','PARSE_CREDENTIALS',function($http,PARSE_CREDENTIALS){
+    return {
+        getAll:function(){
+            return $http.get('https://api.parse.com/1/classes/Todo',{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                }
+            });
+        },
+        get:function(id){
+            return $http.get('https://api.parse.com/1/classes/Todo/'+id,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                }
+            });
+        },
+        create:function(data){
+            return $http.post('https://api.parse.com/1/classes/Todo',data,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                    'Content-Type':'application/json'
+                }
+            });
+        },
+        edit:function(id,data){
+            return $http.put('https://api.parse.com/1/classes/Todo/'+id,data,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                    'Content-Type':'application/json'
+                }
+            });
+        },
+        delete:function(id){
+            return $http.delete('https://api.parse.com/1/classes/Todo/'+id,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                    'Content-Type':'application/json'
+                }
+            });
+        }
+    }
+}])
+.value('PARSE_CREDENTIALS',{
+    APP_ID: '9ehAv41QUFB3K069qrDGc03dVb4BO7qhAvD0bSkV',
+    REST_API_KEY:'rwlHrJzYR9gR2grTcUlCCKK8mqYn1Ao1wySTqhvO'
+})
