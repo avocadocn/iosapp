6ea2a95a1bb0883497b1821880a3de6125a51654
cahavar <cahavar@55yali.com>
2014-12-09 17:25:23 +0800

    添加登录服务

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,39 +1,48 @@
 /**
  * Created by Sandeep on 11/09/14.
  */
-angular.module('donlerApp.controllers',[]).controller('TodoListController',['$scope','Todo',function($scope,Todo){
+angular.module('donlerApp.controllers', []).controller('TodoListController', ['$scope', 'Todo', function ($scope, Todo) {
 
-    Todo.getAll().success(function(data){
-        $scope.items=data.results;
+  Todo.getAll().success(function (data) {
+    $scope.items = data.results;
+  });
+
+  $scope.onItemDelete = function (item) {
+    Todo.delete(item.objectId);
+    $scope.items.splice($scope.items.indexOf(item), 1);
+  }
+
+}]).controller('TodoCreationController', ['$scope', 'Todo', '$state', function ($scope, Todo, $state) {
+
+  $scope.todo = {};
+
+  $scope.create = function () {
+    Todo.create({content: $scope.todo.content}).success(function (data) {
+      $state.go('todos');
     });
-
-    $scope.onItemDelete=function(item){
-        Todo.delete(item.objectId);
-        $scope.items.splice($scope.items.indexOf(item),1);
-    }
-
-}]).controller('TodoCreationController',['$scope','Todo','$state',function($scope,Todo,$state){
-
-    $scope.todo={};
-
-    $scope.create=function(){
-        Todo.create({content:$scope.todo.content}).success(function(data){
-            $state.go('todos');
-        });
-    }
+  }
 
 
-}]).controller('TodoEditController',['$scope','Todo','$state','$stateParams',function($scope,Todo,$state,$stateParams){
+}]).controller('TodoEditController', ['$scope', 'Todo', '$state', '$stateParams', function ($scope, Todo, $state, $stateParams) {
 
-    $scope.todo={id:$stateParams.id,content:$stateParams.content};
+  $scope.todo = {id: $stateParams.id, content: $stateParams.content};
 
-    $scope.edit=function(){
-        Todo.edit($scope.todo.id,{content:$scope.todo.content}).success(function(data){
-            $state.go('todos');
-        });
-    }
+  $scope.edit = function () {
+    Todo.edit($scope.todo.id, {content: $scope.todo.content}).success(function (data) {
+      $state.go('todos');
+    });
+  }
 
-}]).controller('UserLoginController', ['$scope', function ($scope) {
+}]).controller('UserLoginController', ['$scope', 'UserAuth', function ($scope, UserAuth) {
+
+  $scope.email = '';
+  $scope.password = '';
+
+  $scope.login = function () {
+    UserAuth.login($scope.email, $scope.password, function (err) {
+      alert(err);
+    });
+  };
 
 }]).controller('CampaignController', ['$scope', function ($scope) {
 
