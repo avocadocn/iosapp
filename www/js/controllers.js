0c645172affe81cc7045832a4e9ae97144c89923
cahavar <cahavar@55yali.com>
2014-12-10 14:43:33 +0800

    移除todo demo相关代码

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,68 +2,37 @@
  * Created by Sandeep on 11/09/14.
  */
 
-angular.module('donlerApp.controllers', []).controller('TodoListController', ['$scope', 'Todo', function ($scope, Todo) {
+angular.module('donlerApp.controllers', [])
+  .controller('UserLoginController', ['$scope', '$state', 'UserAuth', function ($scope, $state, UserAuth) {
 
-  Todo.getAll().success(function (data) {
-    $scope.items = data.results;
-  });
+    $scope.loginData = {
+      email: '',
+      password: ''
+    };
 
-  $scope.onItemDelete = function (item) {
-    Todo.delete(item.objectId);
-    $scope.items.splice($scope.items.indexOf(item), 1);
-  }
+    $scope.login = function () {
+      UserAuth.login($scope.loginData.email, $scope.loginData.password, function (err) {
+        if (err) {
+          // todo
+          console.log(err);
+        } else {
+          $state.go('campaigns');
+        }
+      });
+    };
 
-}]).controller('TodoCreationController', ['$scope', 'Todo', '$state', function ($scope, Todo, $state) {
+  }])
+  .controller('CampaignController', ['$scope', 'Campaign', function ($scope, Campaign) {
+    // Campaign.getAll('user','').success(function(data){
+    //   $scope.campaigns = data;
+    // })
+  }])
+  .controller('DiscussController', ['$scope', function ($scope) {
 
-  $scope.todo = {};
+  }])
+  .controller('DiscoverController', ['$scope', function ($scope) {
 
-  $scope.create = function () {
-    Todo.create({content: $scope.todo.content}).success(function (data) {
-      $state.go('todos');
-    });
-  }
+  }])
+  .controller('PersonalController', ['$scope', function ($scope) {
 
-
-}]).controller('TodoEditController', ['$scope', 'Todo', '$state', '$stateParams', function ($scope, Todo, $state, $stateParams) {
-
-  $scope.todo = {id: $stateParams.id, content: $stateParams.content};
-
-  $scope.edit = function () {
-    Todo.edit($scope.todo.id, {content: $scope.todo.content}).success(function (data) {
-      $state.go('todos');
-    });
-  }
-
-}]).controller('UserLoginController', ['$scope', '$state', 'UserAuth', function ($scope, $state, UserAuth) {
-
-  $scope.loginData = {
-    email: '',
-    password: ''
-  };
-
-  $scope.login = function () {
-    UserAuth.login($scope.loginData.email, $scope.loginData.password, function (err) {
-      if (err) {
-        // todo
-        console.log(err);
-      } else {
-        $state.go('campaigns');
-      }
-    });
-  };
-
-}])
-.controller('CampaignController', ['$scope', 'Campaign', function ($scope, Campaign) {
-  // Campaign.getAll('user','').success(function(data){
-  //   $scope.campaigns = data;
-  // })
-}])
-.controller('DiscussController', ['$scope', function ($scope) {
-
-}])
-.controller('DiscoverController', ['$scope', function ($scope) {
-
-}])
-.controller('PersonalController', ['$scope', function ($scope) {
-
-}]);
+  }]);
--- www/js/services.js
+++ www/js/services.js
@@ -1,93 +1,44 @@
 /**
  * Created by Sandeep on 11/09/14.
  */
-angular.module('donlerApp.services', []).factory('Todo', ['$http', 'PARSE_CREDENTIALS', function ($http, PARSE_CREDENTIALS) {
-  return {
-    getAll: function () {
-      return $http.get('https://api.parse.com/1/classes/Todo', {
-        headers: {
-          'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
-          'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY,
-        }
-      });
-    },
-    get: function (id) {
-      return $http.get('https://api.parse.com/1/classes/Todo/' + id, {
-        headers: {
-          'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
-          'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY,
-        }
-      });
-    },
-    create: function (data) {
-      return $http.post('https://api.parse.com/1/classes/Todo', data, {
-        headers: {
-          'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
-          'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY,
-          'Content-Type': 'application/json'
-        }
-      });
-    },
-    edit: function (id, data) {
-      return $http.put('https://api.parse.com/1/classes/Todo/' + id, data, {
-        headers: {
-          'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
-          'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY,
-          'Content-Type': 'application/json'
-        }
-      });
-    },
-    delete: function (id) {
-      return $http.delete('https://api.parse.com/1/classes/Todo/' + id, {
-        headers: {
-          'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
-          'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY,
-          'Content-Type': 'application/json'
-        }
-      });
-    }
-  }
-}])
-.factory('UserAuth', ['$http', 'CONFIG', function ($http, CONFIG) {
-  return {
-    login: function (email, password, callback) {
-      $http.post(CONFIG.BASE_URL + '/users/login', {
-        email: email,
-        password: password
-      })
-        .success(function (data, status) {
-          // todo save token
-          callback();
-        })
-        .error(function (data, status) {
-          callback(data.msg);
-        });
-    }
-  };
-}])
-.constant('CONFIG', {
-  BASE_URL: 'http://localhost:3002'
-})
-.factory('Campaign',['$http','CONFIG',function($http,CONFIG){
-    return {
-        getAll:function(type, id){
-            return $http.get(CONFIG.BASE_URL + '/campaigns?requestType=' + type + '&requestId=' + id);
-        },
-        get:function(id){
-            return $http.get(CONFIG.BASE_URL + '/campaigns' + id);
-        },
-        create:function(data){
-            return $http.post(CONFIG.BASE_URL + '/campaigns', data);
-        },
-        edit:function(id,data){
-            return $http.put(BASE_URL+'/campaigns/'+ id, data);
-        },
-        delete:function(id){
-            return $http.delete(CONFIG.BASE_URL + '/campaigns/' + id);
-        }
-    }
-}])
-.value('PARSE_CREDENTIALS',{
-    APP_ID: '9ehAv41QUFB3K069qrDGc03dVb4BO7qhAvD0bSkV',
-    REST_API_KEY: 'rwlHrJzYR9gR2grTcUlCCKK8mqYn1Ao1wySTqhvO'
+angular.module('donlerApp.services', [])
+  .constant('CONFIG', {
+    BASE_URL: 'http://localhost:3002'
   })
+  .factory('UserAuth', ['$http', 'CONFIG', function ($http, CONFIG) {
+    return {
+      login: function (email, password, callback) {
+        $http.post(CONFIG.BASE_URL + '/users/login', {
+          email: email,
+          password: password
+        })
+          .success(function (data, status) {
+            // todo save token
+            callback();
+          })
+          .error(function (data, status) {
+            callback(data.msg);
+          });
+      }
+    };
+  }])
+
+  .factory('Campaign', ['$http', 'CONFIG', function ($http, CONFIG) {
+    return {
+      getAll: function (type, id) {
+        return $http.get(CONFIG.BASE_URL + '/campaigns?requestType=' + type + '&requestId=' + id);
+      },
+      get: function (id) {
+        return $http.get(CONFIG.BASE_URL + '/campaigns' + id);
+      },
+      create: function (data) {
+        return $http.post(CONFIG.BASE_URL + '/campaigns', data);
+      },
+      edit: function (id, data) {
+        return $http.put(BASE_URL + '/campaigns/' + id, data);
+      },
+      delete: function (id) {
+        return $http.delete(CONFIG.BASE_URL + '/campaigns/' + id);
+      }
+    }
+  }])
