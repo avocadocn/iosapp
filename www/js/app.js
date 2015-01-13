3d8de57ab131de12cc803b4953b1c3d6130fd872
afei <afei@55yali.com>
2015-01-13 14:47:42 +0800

    去掉多余的

--- www/js/app.js
+++ www/js/app.js
@@ -10,6 +10,7 @@ angular.module('donlerApp', ['ionic', 'ngCordova', 'donlerApp.controllers', 'don
     $ionicPlatform.ready(function () {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
+      // 
       if (window.cordova && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       }
