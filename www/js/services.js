56fea2fa159ed0a0cbf8a72e44f03e2cf7d698da
cahavar <cahavar@55yali.com>
2015-04-15 15:21:31 +0800

    修正成员列表样式；修改url

--- www/js/services.js
+++ www/js/services.js
@@ -41,9 +41,9 @@ angular.module('donlerApp.services', [])
     $httpProvider.defaults.headers["delete"] = {'Content-Type': 'application/json;charset=utf-8'};
   }])
   .constant('CONFIG', {
-    BASE_URL: 'http://www.55yali.com:3002',
-    STATIC_URL: 'http://www.55yali.com',
-    SOCKET_URL: 'http://www.55yali.com:3005',
+    BASE_URL: 'http://www.donler.com:3002',
+    STATIC_URL: 'http://www.donler.com',
+    SOCKET_URL: 'http://www.donler.com:3005',
     APP_ID: 'id1a2b3c4d5e6f',
     API_KEY: 'key1a2b3c4d5e6f'
   })
