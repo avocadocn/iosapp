6b120be8b3d72ad87f372eee8aee4ee7e57f3ee2
afei <afei@55yali.com>
2015-04-17 18:07:50 +0800

    修改样式，修改比分板,增加loading

--- www/js/directives.js
+++ www/js/directives.js
@@ -789,6 +789,16 @@ angular.module('donlerApp.directives', ['donlerApp.services'])
           if(scope.editing)
             scope.nowTab=nowTab;
         }
+        scope.checkScore = function () {
+          switch(scope.nowTab) {
+            case 'score':
+              return scope.scores[0]==undefined || scope.scores[1]==undefined;
+              break;
+            case 'result':
+              return scope.results[0]==undefined || scope.results[1]==undefined;
+              break;
+          }
+        }
         var getScoreBoardData = function () {
           ScoreBoard.getScore(scope.componentId, function (err, scoreBoardData) {
             if (err) {
@@ -847,7 +857,11 @@ angular.module('donlerApp.directives', ['donlerApp.services'])
         };
         scope.componentId && getScoreBoardData();
         scope.$watch('reloadFlag',function (newVal) {
+
           if(newVal){
+            scope.editing = false;
+            scope.allowEdit = false;
+            scope.allowManage = false;
             if(scope.editModal){
               scope.editModal.remove();
             }
