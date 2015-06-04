3c93f901d49e13c4b74cb5a946cabf5541c61ed9
Maggie <maggie03230@gmail.com>
2015-06-04 14:45:33 +0800

    选图片插件修改

--- addplugin.sh
+++ addplugin.sh
@@ -7,7 +7,7 @@ ionic plugin add de.appplant.cordova.plugin.local-notification
 ionic plugin add https://github.com/VersoSolutions/CordovaClipboard
 ionic plugin add org.apache.cordova.statusbar
 ionic plugin add https://github.com/danwilson/google-analytics-plugin.git
-ionic plugin add https://github.com/wymsee/cordova-imagePicker.git
+ionic plugin add https://github.com/maggie03230/cordova-imagePicker.git
 ionic plugin add org.apache.cordova.geolocation
 ionic plugin add com.ionic.keyboard
 ionic plugin add https://github.com/EddyVerbruggen/LaunchMyApp-PhoneGap-Plugin.git --variable URL_SCHEME=donler
