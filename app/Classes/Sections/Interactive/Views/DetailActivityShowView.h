cac9ca4b6179933cd309cd9812c6356fce5e3b27
gkJason <jason@55yali.com>
2015-07-21 14:09:05 +0800

    活动详情完善，添加头部下拉放大效果

--- app/Classes/Sections/Interactive/Views/DetailActivityShowView.h
+++ app/Classes/Sections/Interactive/Views/DetailActivityShowView.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface DetailActivityShowView : UIScrollView
+@interface DetailActivityShowView : UIView
 
 
 @end
--- app/Classes/Sections/Interactive/Views/DetailActivityShowView.m
+++ app/Classes/Sections/Interactive/Views/DetailActivityShowView.m
@@ -8,6 +8,14 @@
 
 #import "DetailActivityShowView.h"
 
+
+
+@interface DetailActivityShowView()<UIScrollViewDelegate>
+@property (strong,nonatomic) UIScrollView *superView;
+@property (strong,nonatomic) UIImageView *pictureView;
+@property (assign,nonatomic) CGFloat imageViewWidth;
+@property (assign,nonatomic) CGFloat imageViewHeight;
+@end
 @implementation DetailActivityShowView
 
 /*
@@ -24,27 +32,55 @@
     self = [super initWithFrame:frame];
     if (self) {
         [self setFrame:CGRectMake(0, 0, DLScreenWidth, DLScreenHeight)];
-        [self setBounces:YES];
+        
         [self buildInterface];
     }
     return self;
 }
 
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    [self updateImg];
+}
+
+
+- (void)updateImg {
+    CGFloat yOffset   = self.superView.contentOffset.y + 64;
+    // NSLog(@"%f",yOffset);
+    if (yOffset < 0) {
+        
+        CGFloat factor = ((ABS(yOffset)+self.imageViewHeight)*self.imageViewWidth)/self.imageViewHeight;
+        CGRect f = CGRectMake(-(factor-self.imageViewWidth)/2, 0, factor, self.imageViewHeight+ABS(yOffset));
+        self.pictureView.frame = f;
+        self.pictureView.y += yOffset;
+    }
+}
+
 
 -(void)buildInterface{
+    
+    UIScrollView *superView = [[UIScrollView alloc]init];
+    [superView setDelegate:self];
+    [superView setFrame:self.frame];
+    [superView setBounces:YES];
+    
     // 顶部带有图片的视图
     UIView *topPictureView = [[UIView alloc]init];
     [topPictureView setBackgroundColor:[UIColor whiteColor]];
    
     // 添加活动图片
     UIImageView *pictureView = [UIImageView new];
-    [pictureView setFrame:CGRectMake(0, 0, DLScreenWidth, DLScreenWidth * 4/5)];
+    CGFloat imageViewWidth = DLScreenWidth;
+    CGFloat imageViewHeight = imageViewWidth * 4 / 5;
+    self.imageViewWidth = imageViewWidth;
+    self.imageViewHeight = imageViewHeight;
+    [pictureView setFrame:CGRectMake(0, 0, imageViewWidth, imageViewHeight)];
     [pictureView setBackgroundColor:[UIColor redColor]];
     UIImage *img = [UIImage imageNamed:@"2.jpg"];
     [pictureView setClipsToBounds:YES];
     [pictureView setImage:img];
     [pictureView setContentMode:UIViewContentModeScaleAspectFill];
     
+    self.pictureView = pictureView;
     // 活动名称label
     UIFont *font = [UIFont systemFontOfSize:18.0f];
     UILabel *name = [[UILabel alloc]init];
@@ -78,7 +114,7 @@
     topPictureView.width = DLScreenWidth;
     [topPictureView setOrigin:CGPointZero];
     
-    [self addSubview:topPictureView];
+    [superView addSubview:topPictureView];
     
     // 添加中部时间地址view
     UIView *middleView = [[UIView alloc]init];
@@ -86,7 +122,7 @@
     
     // 添加时间label
     UILabel *timeLabel = [[UILabel alloc]init];
-    NSString *timeString = [NSString stringWithFormat:@"时间: %@",@"2015.07.23--2015.07.30"];
+    NSString *timeString = [NSString stringWithFormat:@"时间:  \%@",@"2015.07.23--2015.07.30"];
     
     NSMutableAttributedString *mutableAttrStr = [[NSMutableAttributedString alloc]initWithString:timeString];
     [mutableAttrStr addAttribute:NSForegroundColorAttributeName value:[UIColor grayColor] range:NSMakeRange(0, 3)];
@@ -107,22 +143,147 @@
     divisionLine.y = CGRectGetMaxY(timeLabel.frame);
     
     // 添加地址label
+    UIFont *addressFont = [UIFont systemFontOfSize:13.0f];
+    CGSize TintLabelSize = CGSizeMake(DLScreenWidth, MAXFLOAT);
+    
+    UILabel *addressTintLabel = [[UILabel alloc]init];
+    NSString *addressTintStr = @"地址: ";
+    CGSize addressTintLabelSize = [addressTintStr sizeWithFont:addressFont constrainedToSize:TintLabelSize lineBreakMode:NSLineBreakByWordWrapping];
+    [addressTintLabel setText:addressTintStr];
+    [addressTintLabel setFont:addressFont];
+    [addressTintLabel setTextColor:[UIColor grayColor]];
+    // tintLabel的frame
+    addressTintLabel.size = addressTintLabelSize;
+    addressTintLabel.x = 10;
+    addressTintLabel.y = CGRectGetMaxY(divisionLine.frame) + 12;
+    
     UILabel *addressLabel = [[UILabel alloc]init];
-    //TODO
+    addressLabel.lineBreakMode = UILineBreakModeWordWrap;
+    addressLabel.numberOfLines = 0;
+    // TODO
+    CGSize maxLabelSize = CGSizeMake(DLScreenWidth - 2 * 10 - addressTintLabelSize.width, MAXFLOAT);
+    NSString *addressText = [NSString stringWithFormat:@"%@",@"上海话剧阿斯顿发送到发送到发送到发送到发士大夫撒飞洒的"];
+    
+    CGSize trueLabelSize = [addressText sizeWithFont:addressFont constrainedToSize:maxLabelSize lineBreakMode:NSLineBreakByWordWrapping];
+    [addressLabel setFont:addressFont];
+    NSMutableAttributedString *addressMAS = [[NSMutableAttributedString alloc]initWithString:addressText];
+   
+    [addressLabel setAttributedText:addressMAS];
+    
+    // 设置地址label的frame
+    addressLabel.size = trueLabelSize;
+    addressLabel.x = CGRectGetMaxX(addressTintLabel.frame);
+    addressLabel.y = CGRectGetMaxY(divisionLine.frame) + 12;
+    
+    
+  
+    
+    
+    // 地图view
+    UIImage *mapImg = [UIImage imageNamed:@"map"];
+    UIImageView *mapView = [[UIImageView alloc]initWithImage:mapImg];
+    [mapView setContentMode:UIViewContentModeScaleAspectFill];
+    [mapView setClipsToBounds:YES];
+    
+    // mapView frame
+    mapView.width = DLScreenWidth;
+    mapView.height = DLScreenWidth * 2 / 4;
+    mapView.x = 0;
+    mapView.y = CGRectGetMaxY(addressLabel.frame) + 10;
+    
+    UILabel *personCountLabel = [[UILabel alloc]init];
+    UIFont *personCountLabelFont = [UIFont systemFontOfSize:13.0f];
+    [personCountLabel setFrame:CGRectMake(10, CGRectGetMaxY(mapView.frame) + 15, DLScreenWidth - 2 * 10, 20)];
+    [personCountLabel setFont:personCountLabelFont];
+    NSInteger personCount = 18;
+    NSString *personCountLabelText = [NSString stringWithFormat:@"报名人数:  %zd人",personCount];
+    NSMutableAttributedString *personMAS = [[NSMutableAttributedString alloc]initWithString:personCountLabelText];
+    [personMAS addAttribute:NSForegroundColorAttributeName value:[UIColor grayColor] range:NSMakeRange(0, 5)];
+    [personCountLabel setAttributedText:personMAS];
+    
+    
     
     // 设置中部view的frame
     middleView.y = CGRectGetMaxY(topPictureView.frame) + 12;
     middleView.x = 0;
     middleView.width = DLScreenWidth;
-    middleView.height = 300;
-    
+    middleView.height = CGRectGetMaxY(personCountLabel.frame) + 14;
     
     [middleView addSubview:timeLabel];
     [middleView addSubview:divisionLine];
-    [self addSubview:middleView];
+    [middleView addSubview:addressTintLabel];
+    [middleView addSubview:addressLabel];
+    [middleView addSubview:mapView];
+    [middleView addSubview:personCountLabel];
+    [superView addSubview:middleView];
+    
+    // 添加第三栏的活动介绍view
+    UIView *introduceView = [[UIView alloc]init];
+    [introduceView setBackgroundColor:[UIColor whiteColor]];
+    
+    UILabel *activityIntroduceTV = [[UILabel alloc]init];
+        UIFont *aITVFont = [UIFont systemFontOfSize:13.0f];
+    [activityIntroduceTV setTextColor:[UIColor blackColor]];
+    NSString *IntroduceText = @"Pixel Winch is a screen measurement app with a unique approach. Rather than overlaying complicated controls on top of your existing workflow, it combines aspects of a traditional image editor with the quick access of a modal interface (similar to OS X's Launchpad or Dashboard).Select a region on the screen to load it inside of Pixel Winch. The captured region will automatically magnify and center on your display. Take measurements using any combination of the included tools. When finished, simply press escape to dismiss Pixel Winch.Need to look up previous measurements? No problem  use the history list to revisit past images. Worried about images clogging up your hard drive? Pixel Winch can be configured to automatically delete them.";
+    NSString *aITVText = [NSString stringWithFormat:@"活动介绍\n%@",IntroduceText];
+    // TODO
+    [activityIntroduceTV setText:aITVText];
+    CGSize maxAITVSize = CGSizeMake(DLScreenWidth - 2 * 10, MAXFLOAT);
+    [activityIntroduceTV setFont:aITVFont];
+    CGSize aITVSize = [aITVText sizeWithFont:aITVFont constrainedToSize:maxAITVSize lineBreakMode:NSLineBreakByWordWrapping];
+    //  AITV的frame
+    activityIntroduceTV.size = aITVSize;
+    activityIntroduceTV.x = 10;
+    activityIntroduceTV.y = 6;
+    activityIntroduceTV.lineBreakMode = UILineBreakModeWordWrap;
+    activityIntroduceTV.numberOfLines = 0;
+    
+    introduceView.width = DLScreenWidth;
+    introduceView.height = activityIntroduceTV.height + 16;
+    introduceView.x = 0;
+    introduceView.y = CGRectGetMaxY(middleView.frame) + 12;
+    
+    // 添加到根view中
+    [introduceView addSubview:activityIntroduceTV];
+    [superView addSubview:introduceView];
     
     // 设置contentoffset
-    [self setContentSize:CGSizeMake(DLScreenWidth, CGRectGetMaxY(middleView.frame))];
+    [superView setContentSize:CGSizeMake(DLScreenWidth, CGRectGetMaxY(introduceView.frame) + 44)];
+    
+    
+    
+    [self addSubview:superView];
+    
+    UIView *sighUpView = [[UIView alloc]init];
+    [sighUpView setBackgroundColor:[UIColor redColor]];
+    
+    // 设置报名view的frame
+    sighUpView.size = CGSizeMake(DLScreenWidth, 44);
+    sighUpView.x = 0;
+    sighUpView.y = DLScreenHeight - 44;
+    
+    UIButton *sighUpBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [sighUpBtn setTitle:@"立即报名" forState:UIControlStateNormal];
+    sighUpBtn.width = DLScreenWidth - 2 * 10;
+    sighUpBtn.height = 28;
+    sighUpBtn.x = 10;
+    sighUpBtn.y = sighUpView.height / 2 - sighUpBtn.height / 2;
+    [sighUpBtn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    
+    // btn添加到view中
+    [sighUpView addSubview:sighUpBtn];
+    [self addSubview:sighUpView];
+
+    
+    self.superView = superView;
+    
+   
 }
+
+-(void)btnClick:(id)sender{
+    NSLog(@"btn Clicked");
+}
+
+
 
 @end
