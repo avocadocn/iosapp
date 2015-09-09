c2f375f03fd69aa28101f7a9ec42a45737fddded
tom <tom@55yali.com>
2015-09-09 16:41:09 +0800

    1.添加了活动说明的webview 2.活动展示页面的背景进行了宽度适配 3.添加了一个处理HTML代码的工具类

---
+++ app/Classes/Sections/Interactive/Models/getTemplateModel.h
@@ -0,0 +1,13 @@
+//
+//  getActivityModel.h
+//  app
+//
+//  Created by tom on 15/9/8.
+//  Copyright (c) 2015年 Donler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface getActivityModel : NSObject
+
+@end
