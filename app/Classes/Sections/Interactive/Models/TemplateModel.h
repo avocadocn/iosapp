7b78358b147926316c260f8565c200a83a0d7279
tom <tom@55yali.com>
2015-09-17 16:26:01 +0800

    添加一个描述模板的Model

---
+++ app/Classes/Sections/Interactive/Models/TemplateModel.h
@@ -0,0 +1,13 @@
+//
+//  TemplateModel.h
+//  app
+//
+//  Created by tom on 15/9/17.
+//  Copyright (c) 2015年 Donler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TemplateModel : NSObject
+
+@end
---
+++ app/Classes/Sections/Interactive/Models/TemplateModel.m
@@ -0,0 +1,13 @@
+//
+//  TemplateModel.m
+//  app
+//
+//  Created by tom on 15/9/17.
+//  Copyright (c) 2015年 Donler. All rights reserved.
+//
+
+#import "TemplateModel.h"
+
+@implementation TemplateModel
+
+@end
