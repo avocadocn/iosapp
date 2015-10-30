9efd469f4de3da9b7cb93df037a2b54f0121a9f2
taiyang <1064884357@qq.com>
2015-10-30 17:50:41 +0800

    解决首页选择器停止转动的问题

--- app/Classes/Sections/Interactive/Controllers/InteractiveViewController.m
+++ app/Classes/Sections/Interactive/Controllers/InteractiveViewController.m
@@ -47,6 +47,7 @@ enum InteractionType{
     InteractionTypeHelpTemplate
 };
 
+
 @interface InteractiveViewController ()<ActivitysShowViewDelegate,UITableViewDataSource,UITableViewDelegate,DCPathButtonDelegate, DWBubbleMenuViewDelegate, UIAlertViewDelegate>
 
 @property (nonatomic ,strong) ActivitysShowView *asv;
@@ -73,35 +74,38 @@ enum InteractionType{
 
 @implementation InteractiveViewController
 
+
+
 static NSString * const ID = @"CurrentActivitysShowCell";
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         [self loadingContacts]; // 加载通讯录信息
     });
- 
+    
     // Do any additional setup after loading the view.
-
+    
     UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, DLScreenWidth, 24)];
     
     view.backgroundColor = [UIColor greenColor];
     [self.navigationController.toolbar addSubview:view];
-
+    
     // 设置背景颜色
     [self.view setBackgroundColor:RGB(235, 235, 235)];
     
     // 上方活动展示view
     [self setupActivitysShowView];
     
-  //  NSLog(@"%@",[UIApplication sharedApplication].keyWindow.rootViewController);
+    //  NSLog(@"%@",[UIApplication sharedApplication].keyWindow.rootViewController);
     
     
     // 活动展示table
     [self setupActivityShowTableView];
     [self requestNet];
-//     [self loadData]; // 加载数据
+    //     [self loadData]; // 加载数据
     [self refressMJ]; // 下拉刷新 上拉加载
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reFreshData) name:@"KPOSTNAME" object:nil];
@@ -115,30 +119,30 @@ static NSString * const ID = @"CurrentActivitysShowCell";
 
 
 /*
-- (void)loadData { // 加载数据 判断本地是否已经存在数据
-    NSFileManager *manger = [NSFileManager defaultManager];
-    NSString *cachePath = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) firstObject];
-    self.path = [cachePath stringByAppendingPathComponent:@"modelArray"];
-    self.modelArray = [NSMutableArray arrayWithContentsOfFile:[cachePath stringByAppendingPathComponent:@"modelArray"]];
-//    
-//    BOOL orTrue = [manger fileExistsAtPath:self.path];
-//    if (orTrue) {
-//        self.modelArray = [NSMutableArray arrayWithContentsOfFile:path];
-//        if (self.modelArray.count == 0) {
-//            [self requestNet];
-//        } else {
-//            
-//        }
-//    } else {
-//        [self requestNet];
-//    }
-    if (self.modelArray.count == 0) {
-        [self requestNet];
-    } else {
-        [self analyDataWithJson:self.modelArray];
-    }
+ - (void)loadData { // 加载数据 判断本地是否已经存在数据
+ NSFileManager *manger = [NSFileManager defaultManager];
+ NSString *cachePath = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) firstObject];
+ self.path = [cachePath stringByAppendingPathComponent:@"modelArray"];
+ self.modelArray = [NSMutableArray arrayWithContentsOfFile:[cachePath stringByAppendingPathComponent:@"modelArray"]];
+ //
+ //    BOOL orTrue = [manger fileExistsAtPath:self.path];
+ //    if (orTrue) {
+ //        self.modelArray = [NSMutableArray arrayWithContentsOfFile:path];
+ //        if (self.modelArray.count == 0) {
+ //            [self requestNet];
+ //        } else {
+ //
+ //        }
+ //    } else {
+ //        [self requestNet];
+ //    }
+ if (self.modelArray.count == 0) {
+ [self requestNet];
+ } else {
+ [self analyDataWithJson:self.modelArray];
+ }
  
-}
+ }
  */
 - (void)reFreshData {
     
@@ -150,14 +154,14 @@ static NSString * const ID = @"CurrentActivitysShowCell";
     [RestfulAPIRequestTool routeName:@"getInteraction" requestModel:model useKeys:@[@"interactionType", @"requestType", @"createTime", @"limit", @"userId"] success:^(id json) {
         NSLog(@"获取成功   %@", json);
         [self analyDataWithJson:json];
-
+        
     } failure:^(id errorJson) {
         NSLog(@"获取失败  %@", errorJson);
         NSString *str = [errorJson objectForKey:@"msg"];
         if ([str isEqualToString:@"您没有登录或者登录超时，请重新登录"]) {
             UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"身份信息过期" message:@"您没有登录或者登录超时，请重新登录" delegate:self cancelButtonTitle:@"好的" otherButtonTitles:nil, nil];
             alert.delegate = self;
-
+            
             [alert show];
             
         }
@@ -170,8 +174,12 @@ static NSString * const ID = @"CurrentActivitysShowCell";
 
 -(void)viewWillAppear:(BOOL)animated{
     // 弹出式菜单
-//    [self setupPathButton];
-    [self builtInterface];
+    //    [self setupPathButton];
+    if (self.coriusView.frame.size.width == 0) {
+        [self builtInterface];
+    }
+    
+    //    [self builtInterface];
     
 }
 - (void)dismissAction:(UITapGestureRecognizer *)sender
@@ -212,20 +220,21 @@ static NSString * const ID = @"CurrentActivitysShowCell";
 - (void)builtInterface
 {
     UILabel *homeLabel = [self createHomeButtonView];
-    if (!self.upMenuView) {
-        
+    if (self.upMenuView) {
+        [self.upMenuView removeFromSuperview];
+    }
     self.upMenuView = [[DWBubbleMenuButton alloc] initWithFrame:CGRectMake([UIScreen mainScreen].bounds.size.width - homeLabel.frame.size.width - 10.f,
-                                                                                          [UIScreen mainScreen].bounds.size.height
-                                                                                          - homeLabel.frame.size.height - DLMultipleHeight(72.0),
-                                                                                          homeLabel.frame.size.width,
-                                                                                          homeLabel.frame.size.height)
-                                                            expansionDirection:DirectionUp];
+                                                                           [UIScreen mainScreen].bounds.size.height
+                                                                           - homeLabel.frame.size.height - DLMultipleHeight(72.0),
+                                                                           homeLabel.frame.size.width,
+                                                                           homeLabel.frame.size.height)
+                                             expansionDirection:DirectionUp];
     self.upMenuView.homeButtonView = homeLabel;
     self.upMenuView.delegate = self;
     [self.upMenuView addButtons:[self createDemoButtonArray]];
     
     [self.view addSubview:self.upMenuView];
-    }
+    //    }
 }
 - (UILabel *)createHomeButtonView {
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0.f, 0.f, DLMultipleWidth(96.0), DLMultipleWidth(48.0))];
@@ -372,16 +381,16 @@ static NSString * const ID = @"CurrentActivitysShowCell";
  */
 -(void)hiddenASV{
     self.asvHidden = YES;
-
+    
     [UIView animateWithDuration:0.2f animations:^{
         self.asv.y -= 100;
         self.tableView.y -= 100 ;
         self.tableView.height +=100;
         self.asv.alpha = 0;
-
+        
     } completion:^(BOOL finished) {
         [UIView animateWithDuration:0.1f animations:^{
-//             self.navigationController.navigationBar.height = 0;
+            //             self.navigationController.navigationBar.height = 0;
         }];
     }];
 }
@@ -391,17 +400,17 @@ static NSString * const ID = @"CurrentActivitysShowCell";
  */
 -(void)showASV{
     self.asvHidden = NO;
-[UIView animateWithDuration:0.2f animations:^{
-    self.asv.alpha = 1;
-    self.asv.y += 100 ;
-    self.tableView.y += 100 ;
-    self.tableView.height -= 100 ;
-    
-} completion:^(BOOL finished) {
     [UIView animateWithDuration:0.2f animations:^{
+        self.asv.alpha = 1;
+        self.asv.y += 100 ;
+        self.tableView.y += 100 ;
+        self.tableView.height -= 100 ;
         
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.2f animations:^{
+            
+        }];
     }];
-}];
 }
 
 
@@ -418,8 +427,8 @@ static NSString * const ID = @"CurrentActivitysShowCell";
         case 0: // 男神
         {
             RankListController  *controller =  [[RankListController alloc]initWithRankListType:RankListTypeMenGod];
-//            NewRankListControllerViewController * controller = [[NewRankListControllerViewController alloc] initWithNibName:@"NewRankListControllerViewController" bundle:nil];
-//            controller.listType = NewRankListTypeMenGod;
+            //            NewRankListControllerViewController * controller = [[NewRankListControllerViewController alloc] initWithNibName:@"NewRankListControllerViewController" bundle:nil];
+            //            controller.listType = NewRankListTypeMenGod;
             [self.navigationController pushViewController:controller animated:YES];
         }
             break;
@@ -439,13 +448,13 @@ static NSString * const ID = @"CurrentActivitysShowCell";
             break;
         case 3: // 什么活动
         {
-
+            
             OtherController *twitterPaggingViewer = [[OtherController alloc]init];
             
             
             NSMutableArray *viewControllers = [[NSMutableArray alloc] initWithCapacity:7];
             //暂时屏蔽掉求助
-//            NSArray *titles = @[@"活动", @"投票", @"求助"];
+            //            NSArray *titles = @[@"活动", @"投票", @"求助"];
             NSArray *titles = @[@"活动", @"投票"];
             [titles enumerateObjectsUsingBlock:^(NSString *title, NSUInteger idx, BOOL *stop) {
                 if (idx == InteractionTypeActivityTemplate) {
@@ -469,15 +478,15 @@ static NSString * const ID = @"CurrentActivitysShowCell";
             twitterPaggingViewer.viewControllers = viewControllers;
             
             twitterPaggingViewer.didChangedPageCompleted = ^(NSInteger cuurentPage, NSString *title) {
-              
+                
             };
-
+            
             
             [self.navigationController pushViewController:twitterPaggingViewer animated:YES];
         }
             break;
             
-        
+            
         default:
             break;
     }
@@ -552,10 +561,10 @@ static NSString * const ID = @"CurrentActivitysShowCell";
     [RestfulAPIRequestTool routeName:@"getInteraction" requestModel:model useKeys:@[@"interactionType", @"requestType", @"createTime", @"limit", @"userId"] success:^(id json) {
         NSLog(@"获取成功   %@", json);
         [self analyDataWithJson:json];
-
+        
     } failure:^(id errorJson) {
         NSLog(@"获取失败  %@", errorJson);
-   
+        
         NSString *str = [errorJson objectForKey:@"msg"];
         if ([str isEqualToString:@"您没有登录或者登录超时，请重新登录"]) {
             UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"身份信息过期" message:@"您没有登录或者登录超时，请重新登录" delegate:self cancelButtonTitle:@"好的" otherButtonTitles:nil, nil];
@@ -580,11 +589,11 @@ static NSString * const ID = @"CurrentActivitysShowCell";
             [poll setValuesForKeysWithDictionary:[dic objectForKey:@"poll"]];
             [inter setPoll:poll];
         }
-
+        
         [self.modelArray addObject:inter];
     }
     [self.tableView reloadData];
-//    [self.modelArray writeToFile:[self.path stringByAppendingPathComponent:@"modelArray"]atomically:YES];
+    //    [self.modelArray writeToFile:[self.path stringByAppendingPathComponent:@"modelArray"]atomically:YES];
 }
 - (void)testTapAction:(UITapGestureRecognizer *)sender {
     
@@ -671,13 +680,13 @@ static NSString * const ID = @"CurrentActivitysShowCell";
         [self fireTimerWithBool:YES];
     });
     Account *acc= [AccountTool account];
-       Interaction *imodel = [self.modelArray lastObject];
+    Interaction *imodel = [self.modelArray lastObject];
     getIntroModel *model = [[getIntroModel alloc]init];
     [model setUserId:acc.ID];
     [model setLimit:@10];
     [model setCreateTime:imodel.createTime];
     [RestfulAPIRequestTool routeName:@"getInteraction" requestModel:model useKeys:@[@"interactionType", @"requestType", @"createTime", @"limit", @"userId"] success:^(id json) {
-         NSLog(@"获取成功   %@", json);
+        NSLog(@"获取成功   %@", json);
         [self.tableView.footer endRefreshing];
         [self dealDataWithJson:json];
         [self.tableView reloadData];
