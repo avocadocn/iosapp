0cd5679b54caa4fafdc6ca0b074651f3c59d5bf2
tom <tom@55yali.com>
2015-10-30 14:52:16 +0800

    添加一个Label自适应内容方法

---
+++ app/Classes/Helpers/UILabel+FitContent.h
@@ -0,0 +1,13 @@
+//
+//  UILabel+FitContent.h
+//  test
+//
+//  Created by tom on 15/10/30.
+//  Copyright © 2015年 tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (FitContent)
+- (void)fitContent;
+@end
